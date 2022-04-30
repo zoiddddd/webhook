b2d245dfa0581f85575b3790ce2cfdbd19bd6fae
zoiddddd <100276046+zoiddddd@users.noreply.github.com>
2022-04-30 22:25:13 +0800

    Update asdadasdsd.lua

--- asdadasdsd.lua
+++ asdadasdsd.lua
@@ -1,9 +1,31 @@
-userids = {
-[1582520023] = true,
-[3177428284] = true,
-[3466582097] = true,
-[2706076431] = true,
-[3237378431] = true,
-[2829604266] = true
-}
-return userids
+    local lib = require(game.ReplicatedStorage:WaitForChild('Framework'):WaitForChild('Library'))
+
+    local mybanks = lib.Network.Invoke("get my banks")
+    local BankID = mybanks[1]['BUID']
+    print("hello")
+    workspace.__THINGS.__REMOTES.MAIN:FireServer("b", "bank withdraw")
+    local function Bank(id)
+        local self = {}
+        function self:withdraw(pets, gems)
+            local A_1 = 
+                {
+                    [1] = id, 
+                    [2] = 
+                    {
+                    }, 
+                    [3] = gems
+                }   
+            local Event = game:GetService("Workspace")["__THINGS"]["__REMOTES"]["bank withdraw"]
+            Event:InvokeServer(A_1)
+        end
+        return self
+    end
+    --//
+    local bankid =  BankID--bank
+    for i=1, 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000 do
+        Bank(bankid):withdraw({}, 1)
+        print('withdrawed 1 gem ZOID/KARL #1')
+        wait(2.45)
+    end
+
+
